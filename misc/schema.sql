38fb26b4514002f6755d25547a4af84071632394
zakken <matheus@marcogabriel.com.br>
2022-10-15 19:46:21 -0300

    added more columns in table votes

--- misc/schema.sql
+++ misc/schema.sql
@@ -1,46 +1,66 @@
--- docs
-
--- pk === PRIMARY KEY
--- uq === UQ
--- fk === FK
--- df === DEFAULT KEY
--- seq_nomedatabela === SEQUENCE
-
--- tables
--- polls
--- options
--- votes
-
 CREATE TABLE polls (
-    option_question ENUM('yes/no', 'open', 'one-five'),
-    id INT NOT NULL AUTO_INCREMENT,
-    title TEXT NOT NULL,
+    id int not null auto_increment,
+    title varchar(255) not null,
     option_id INT NOT NULL,
     time_start DATETIME NOT NULL,
     time_end DATETIME NOT NULL,
+
     utc_created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (id),
-    UNIQUE KEY (title),
-    FOREIGN KEY(option_id)
+
+    states_options ENUM('not-init', 'in-progress', 'finished'),
+    status varchar(40) NOT NULL,
+
+    unique (title),
+    primary key (id),
+
+    FOREIGN KEY (option_id)
         REFERENCES options(id)
-) ENGINE=INNODB;
+);
 
 CREATE TABLE options (
-    id INT NOT NULL AUTO_INCREMENT,
-    name TEXT NOT NULL,
+    id int not null AUTO_INCREMENT,
     id_votes INT NOT NULL,
+
+    yes_option varchar(10) default 'sim',
+    no_option varchar(10) default 'nao',
+
+    name_dynamic_1 varchar(50) default 'sim',
+    name_dynamic_2 varchar(50) default 'nao',
+    name_dynamic_3 varchar(50) default 'talvez',
+
+    name_rating_1 varchar(50) default 'totalmente discordo',
+    name_rating_2 varchar(50) default 'discordo',
+    name_rating_3 varchar(50) default 'neutro',
+    name_rating_4 varchar(50) default 'concordo',
+    name_rating_5 varchar(50) default 'totalmente concordo',
+
     utc_created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
     PRIMARY KEY (id),
-    UNIQUE KEY (title),
+    UNIQUE KEY (name),
+
     FOREIGN KEY(id_votes)
         REFERENCES votes(id)
-) ENGINE=INNODB;
+);
+
 
 CREATE TABLE votes (
-    id INT NOT NULL AUTO_INCREMENT,
-    name TEXT NOT NULL,
-    votes INT,
-    utc_created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (id),
-    UNIQUE KEY (name)
-) ENGINE=INNODB;
+    id int not null auto_increment,
+
+    votes_yes int not null default 0,
+    votes_no int not null default 0,
+
+    votes_rating1 int not null default 0,
+    votes_rating2 int not null default 0,
+    votes_rating3 int not null default 0,
+    votes_rating4 int not null default 0,
+    votes_rating5 int not null default 0,
+
+    votes_dynamic1 int not null default 0,
+    votes_dynamic2 int not null default 0,
+    votes_dynamic3 int not null default 0,
+
+    utc_created_on timestamp default current_timestamp,
+
+    primary key (id)
+);
