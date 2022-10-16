e95baba16e8dc84d6e6ebb477be2b5a458da7cb6
zakken <matheus@marcogabriel.com.br>
2022-10-16 19:20:13 -0300

    All console.log is replaced by logger and a make a functionality to bind all requests

--- server/api/api.js
+++ server/api/api.js
@@ -6,27 +6,49 @@ const app = express();
 
 process.env.TZ = 'UTC';
 
+const logger = require('../shared/logger');
+
 function fatalHandler(err) {
-    console.log(err, { FATAL: true });
+    logger.error(err, { FATAL: true });
     process.exit(1);
 }
 
 process.on('uncaughtException', fatalHandler);
 process.on('unhandleRejection', fatalHandler);
 
+app.use((req, res, next) => {
+    const { ip, method, url, statusCode } = req
+
+    const id = new Date().getTime()
+    const msg = `[${ip} ${method} ${id} - receiving {${url}}]`
+
+    logger.info(msg);
+
+    res.on('close', () => {
+        const start = new Date() - id;
+
+        logger.info(`[${ip}{${method} ${id} - ]` + 
+            `closed: ${url} ${statusCode} ${start}ms`
+        )
+    });
+
+    next();
+})
+
 require('./routes')(app);
 
 let hasInitializedServer = false;
 let httpServer;
+
 
 function listen() {
     httpServer = app.listen(process.env.HTTP_PORT, () => {
         if (hasInitializedServer) { return; }
 
         hasInitializedServer = true;
-        console.log(`it's alive http://localhost:${process.env.HTTP_PORT}`)
+        logger.info(`it's alive http://localhost:${process.env.HTTP_PORT}`)
     }).on('error', (err) => {
-        console.log(err);
+        logger.error(err);
         process.exit(1);
     });
 }
