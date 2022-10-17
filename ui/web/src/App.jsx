4cfac4083e02eb5984fb61f27a65de8b0e894c89
zakken <matheus@marcogabriel.com.br>
2022-10-17 20:22:26 -0300

    Added /form a route to create the polls

--- ui/web/src/App.jsx
+++ ui/web/src/App.jsx
@@ -9,6 +9,8 @@ import './assets/root.css';
 
 import Navbar from './components/Navbar';
 
+import FormPage from './pages/FormPage';
+
 function HomePage() {
     return (
         <h1>Home!</h1>
@@ -22,6 +24,7 @@ function App() {
           <Navbar />
           <Routes>
               <Route path='/' element={ <HomePage /> }></Route>
+              <Route path="/form" element={ <FormPage /> }></Route>
           </Routes>
       </BrowserRouter>
   )
